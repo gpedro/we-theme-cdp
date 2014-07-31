d525d26592dbadb01b66899dc221b0aa20dbb3f6
Daniel Torres <danielfeelfine@gmail.com>
2014-07-30 23:43:14 -0300

    jquery via bower. Closes #27

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,7 +63,7 @@ module.exports = function (grunt) {
     // https://github.com/gruntjs/grunt-contrib-concat
     concat: {
       dist: {
-        src: ['<%= path.src %>/javascript/jquery.js',
+        src: ['<%= path.bower %>/jquery/dist/jquery.js',
               '<%= path.src %>/javascript/bootstrap.js'],
         dest: '<%= path.dist %>/js/scripts.js',
       },
