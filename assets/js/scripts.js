91321db8578b2e863b482e92894b851af7197beb
Alberto Souza <alberto.souza.99@gmail.com>
2014-09-10 00:31:58 -0300

    updated the translations and user.index structure

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,12 @@
 /* main scripts */
+
+// tootip configs
+$('#tooltip').tooltip('hide');
+
+// Ember breadcrumbs configs
+window.BreadCrumbs.BreadCrumbsComponent.reopen({
+  tagName: 'ol',
+  classNames: ['breadcrumb'],
+  // use application/templates/components/bread-crumbs.hbs template
+  layout: null
+});
